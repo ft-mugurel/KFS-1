3824444a192ee0d98cc13ebf39f424144ff12b91
Muhammed Talha Ugurel <muhammedtalhaugurel@gmail.com>
2025-03-29 18:26:21 +0300

    FIX Split vga file to modules

--- kernel/src/vga/text_mod/out.rs
+++ kernel/src/vga/text_mod/out.rs
@@ -61,6 +61,10 @@ pub fn print(str: &str, color: ColorCode) {
 }
 
 #[allow(dead_code)]
+pub fn newline() {
+}
+
+#[allow(dead_code)]
 pub fn scroll() {
     for x in 0..VGA_HEIGHT { 
         for  y in 0..VGA_WIDTH {
@@ -71,40 +75,5 @@ pub fn scroll() {
                 VGA_BUFFER.offset(index as isize).write_volatile(vga_char);
             }
         }
-    }
-}
-use x86::io::{outb, inb};
-
-const VGA_CMD_PORT: u16 = 0x3D4;
-const VGA_DATA_PORT: u16 = 0x3D5;
-
-#[allow(dead_code)]
-pub fn set_big_cursor() {
-    unsafe {
-        outb(VGA_CMD_PORT, 0x0A);
-        outb(VGA_DATA_PORT, 0x00);
-        outb(VGA_CMD_PORT, 0x0B);
-        outb(VGA_DATA_PORT, 0x0F);
-    }
-}
-
-#[allow(dead_code)]
-pub fn move_cursor(x: u16, y: u16) {
-    let position = y * 80 + x;
-
-    unsafe {
-        outb(VGA_CMD_PORT, 0x0E);
-        outb(VGA_DATA_PORT, (position >> 8) as u8);
-        outb(VGA_CMD_PORT, 0x0F);
-        outb(VGA_DATA_PORT, (position & 0xFF) as u8);
-    }
-}
-
-#[allow(dead_code)]
-pub fn disable_cursor() {
-    unsafe {
-        outb(VGA_CMD_PORT, 0x0A);
-        let cursor_start = inb(VGA_DATA_PORT);
-        outb(VGA_DATA_PORT, cursor_start | 0x20);
     }
 }
