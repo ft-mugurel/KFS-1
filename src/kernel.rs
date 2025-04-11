d4ef0d1667c82559b9c78d2e08306effa2fd0a46
Muhammed Talha Ugurel <muhammedtalhaugurel@gmail.com>
2025-04-11 22:53:41 +0300

    ADD print macro
    not done yet but started writing print macro

--- src/kernel.rs
+++ src/kernel.rs
@@ -14,6 +14,7 @@ use interrupts::keyboard::init::init_keyboard;
 use interrupts::idt::init_idt;
 use interrupts::pic::init_pic;
 use interrupts::utils::enable_interrupts;
+use vga::text_mod::out::{print, Color, ColorCode};
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -27,6 +28,7 @@ pub extern "C" fn kmain() -> ! {
     unsafe {init_pic()};
     init_keyboard();
     enable_interrupts();
+    print("test", ColorCode::new(Color::White, Color::Black));
     loop {}
 }
 
--- src/vga/text_mod/print.rs
+++ src/vga/text_mod/print.rs
@@ -1,40 +1,8 @@
-use core::fmt::{self, Write};
-use crate::vga::text_mod::cursor::{move_cursor, set_cursor_x, CURSOR};
-use crate::vga::text_mod::out::{VGA_BUFFER, VGA_WIDTH, ColorCode};
-
-pub struct VgaWriter {
-    pub color: ColorCode,
-}
-
-impl Write for VgaWriter {
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        for byte in s.bytes() {
-            if byte == b'\n' {
-                set_cursor_x(0);
-                move_cursor(0, 1);
-                continue;
-            }
-            let vga_char = (byte as u16) | (self.color.0 as u16) << 8;
-            unsafe {
-                VGA_BUFFER
-                    .offset((CURSOR.y * (VGA_WIDTH as u16) + CURSOR.x) as isize)
-                    .write_volatile(vga_char);
-            }
-            move_cursor(1, 0);
-        }
-        Ok(())
-    }
-}
-
-pub fn print_fmt(args: fmt::Arguments, color: ColorCode) {
-    let mut writer = VgaWriter { color };
-    writer.write_fmt(args).unwrap();
-}
-
 #[macro_export]
 macro_rules! print {
     ($($arg:tt)*) => ({
-        use core::fmt::Write;
-        $crate::vga::text_mod::print_fmt(format_args!($($arg)*), ColorCode::new(Color::White, Color::Black));
+        use crate::vga::text_mod::out::{print, ColorCode, Color};
+        let _ = writeln!(&mut io::stdout(), $($arg)*);
     });
 }
+
